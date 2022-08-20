de7e2ac65d81b66d9929fe972636d9d07a3d6cc5
Siddharth <officialsiddharthbisht@gmail.com>
2022-08-21 02:13:22 +0530

    UPDATE: defined actions and reducers

---
+++ src/store/auth/actions.js
@@ -0,0 +1,59 @@
+import {SIGNUP_ERROR , SIGNUP_SUCCESS ,SIGNUP_LOADING, LOGIN_ERROR ,LOGIN_SUCCESS , LOGIN_LOADING, LOGOUT, FEED_USERDETAILS, FEED_USERDETAILS_ERROR} from './actionTypes';
+
+//--------------Signup --------------- 
+export const signupLoading = () =>{
+    return {
+        type : SIGNUP_LOADING
+    };
+}
+
+export const signupSuccess = () =>{
+    return {
+        type : SIGNUP_SUCCESS
+    };
+}
+
+export const signupError = () =>{
+    return{
+        type : SIGNUP_ERROR
+    }
+}
+
+// -------------Login----------------
+export const loginLoading = () =>{
+    return {
+        type : LOGIN_LOADING
+    };
+}
+
+export const loginSuccess = (payload) =>{
+    return {
+        type : LOGIN_SUCCESS,
+        payload
+    };
+}
+
+export const loginError = () =>{
+    return{
+        type : LOGIN_ERROR
+    }
+}
+
+// ----------------------
+export const logout = () =>{
+    return{
+        type : LOGOUT
+    }
+}
+export const feedUserDetails = (payload) =>{
+    return{
+        type: FEED_USERDETAILS,
+        payload
+    }
+}
+
+export const feedUserDetailsError = () =>{
+    return{
+        type : FEED_USERDETAILS_ERROR
+    }
+}
---
+++ src/store/auth/reducer.js
@@ -0,0 +1,68 @@
+import { SIGNUP_ERROR, SIGNUP_LOADING, SIGNUP_SUCCESS, LOGIN_ERROR, LOGIN_LOADING, LOGIN_SUCCESS, LOGOUT, FEED_USERDETAILS, FEED_USERDETAILS_ERROR } from './actionTypes';
+
+const initialState = {
+    loading: false,
+    error: false,
+    token: null,
+    message: "",
+    userDetails: {}
+}
+
+export const reducer = (state = initialState, { type, payload }) => {
+    switch (type) {
+        case SIGNUP_LOADING:
+            return {
+                ...state,
+                loading: true
+            }
+        case SIGNUP_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                error: false
+            }
+        case SIGNUP_ERROR:
+            return {
+                ...state,
+                loading: false,
+                error: true
+            }
+        case LOGIN_LOADING:
+            return {
+                ...state,
+                loading: true
+            }
+        case LOGIN_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                error: false,
+                token: payload
+            }
+        case LOGIN_ERROR:
+            return {
+                ...state,
+                loading: false,
+                error: true
+            }
+        // _________________________________
+        case LOGOUT:
+            return {
+                ...initialState,
+
+            }
+        case FEED_USERDETAILS:
+            return {
+                ...state,
+                error: false,
+                userDetails: payload
+            }
+        case FEED_USERDETAILS_ERROR:
+            return {
+                ...state,
+                error: true,
+            }
+        default:
+            return state;
+    }
+}
