29abb8ebe7f322554a1b2e7fc2482ca3692739ff
Siddharth <officialsiddharthbisht@gmail.com>
2022-08-21 02:01:30 +0530

    ADD: Input box ,FUNCTIONS: handleSignup (half) handleChange

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,9 +1,64 @@
+import axios from "axios";
 import React from "react";
 
+const initialState = {
+    name : "",
+    email : "",
+    password : "",
+    username : "",
+    mobile : "",
+    description : ""
+}
+
 export const Signup = () =>{
+    const [signupData, setSignupData] = React.useState(initialState);
+
+    const handleChange = (e) =>{
+        const {name , value} = e.target;
+        setSignupData(prev =>(
+            {...prev , [name] : value}
+        ))
+    }
+    const handleSignup = () =>{
+        let isValid = true;
+        Object.values(signupData).forEach(el => {
+            if(!el){
+                isValid = false;
+            }
+        })
+        if(!isValid){
+            alert("Fill all the details please");
+            return;
+        }
+        axios({
+            method : "post",
+            url : "https://masai-api-mocker.herokuapp.com//auth/register",
+            data : signupData
+        }).then(res =>{
+
+        }).catch(err =>{
+            
+        })
+    }
     return(
         <>
-            Signup
+            {Object.keys(signupData).map(el =>(
+                <div key={el}>
+                    <input key = {el} onChange = {handleChange} name = {el} type="text" placeholder={el} value = {signupData[el]}/>
+                </div>
+            ))}
+            <button onClick={handleSignup}>Sign Up</button>
         </>
     )
 }
+
+/*
+{
+  "name": "MASAI School",
+  "email": "hello@masai.com"
+  "password": "secret",
+  "username": "masai-school",
+  "mobile": "9876543210",
+  "description": "A Transformation in education!" 
+}
+*/
